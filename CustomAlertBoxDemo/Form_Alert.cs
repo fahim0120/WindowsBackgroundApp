e084de7218cea935147af99950f6c690f381f010
fahim0120 <fahim0120@gmail.com>
2020-11-04 05:31:54 -0500

    Restart done. To do: HTTP & Install

--- CustomAlertBoxDemo/Form_Alert.cs
+++ CustomAlertBoxDemo/Form_Alert.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -91,6 +93,7 @@ namespace CustomAlertBoxDemo
             // Send HTTP DATA
 
             // RESTART
+            Process.Start("shutdown.exe", "-r -t 00");
         }
 
         public void showAlert(string title, string msg, enmType type)
