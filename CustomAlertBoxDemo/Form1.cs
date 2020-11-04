dcc000223601b797c882cf36b76583c21e82b1ba
fahim0120 <fahim0120@gmail.com>
2020-11-04 05:03:51 -0500

    To do: Restart & Http

---
+++ CustomAlertBoxDemo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomAlertBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Alert(string title, string msg, Form_Alert.enmType type)
+        {
+            Form_Alert frm = new Form_Alert();
+            frm.showAlert(title, msg, type);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Alert("Your computer health 3 * out of 5 *", 
+                "Updating can make the health 5*. Restart Required.", 
+                Form_Alert.enmType.Success);
+        }
+    }
+}
---
+++ CustomAlertBoxDemo/Form_Alert.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CustomAlertBoxDemo
+{
+    partial class Form_Alert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnPostpone = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(15, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(218, 56);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title Text";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CustomAlertBoxDemo.Properties.Resources.icons8_cancel_25px;
+            this.pictureBox2.Location = new System.Drawing.Point(439, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 30);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(45, 90);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(180, 50);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.TabStop = false;
+            this.btnUpdate.Text = "Update Now";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnPostpone
+            // 
+            this.btnPostpone.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnPostpone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPostpone.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPostpone.ForeColor = System.Drawing.Color.White;
+            this.btnPostpone.Location = new System.Drawing.Point(285, 90);
+            this.btnPostpone.Margin = new System.Windows.Forms.Padding(5);
+            this.btnPostpone.Name = "btnPostpone";
+            this.btnPostpone.Size = new System.Drawing.Size(180, 50);
+            this.btnPostpone.TabIndex = 5;
+            this.btnPostpone.TabStop = false;
+            this.btnPostpone.Text = "Postpone";
+            this.btnPostpone.UseVisualStyleBackColor = false;
+            this.btnPostpone.Click += new System.EventHandler(this.btnPostpone_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.ForeColor = System.Drawing.Color.White;
+            this.lblMsg.Location = new System.Drawing.Point(15, 50);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(142, 49);
+            this.lblMsg.TabIndex = 6;
+            this.lblMsg.Text = "label1";
+            // 
+            // Form_Alert
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(500, 150);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnPostpone);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Alert";
+            this.Text = "Form_Alert";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        internal System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnPostpone;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
